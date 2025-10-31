0e9632211ee86dd7d86861bd0f9ffb91d41fcbb3
oguzkaanyilmaz34 <oguzkaanyilmaz34@gmail.com>
2025-10-31 11:37:29 +0300

    Update app_OKY.py

--- app_OKY.py
+++ app_OKY.py
@@ -4,7 +4,7 @@ import math
 st.title("OKY's Alopecia Areata Risk Hesaplayıcı")
 
 # --- Kullanıcıdan veri al ---
-calreticulin = st.number_input("Calreticulin düzeyi (µg/mL)", min_value=0.0, max_value=6.0, step=0.1, format="%.2f")
+calreticulin = st.number_input("Calreticulin düzeyi 0-10 Arası (µg/mL)", min_value=0.0, max_value=6.0, step=0.1, format="%.2f")
 aso10 = st.number_input("ASÖ-10 skoru", min_value=0.0, max_value=40.0, step=1.0, format="%.0f")
 ai_hastalik_yok = st.selectbox("Otoimmün hastalık", ["Var", "Yok"]) == "Yok"
 cinsiyet_kadin = st.selectbox("Cinsiyet", ["Erkek", "Kadın"]) == "Kadın"
